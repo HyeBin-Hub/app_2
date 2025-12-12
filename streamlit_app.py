a8f5af36c21fc3064fb6893c2c594303a5a4bf6a
HyeBin-Hub <gpqls00415@gmail.com>
2025-12-12 20:30:55 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.set_page_config(
 # Header 설정 
 ######################
 st.title("Storyboard Generator")
-st.caption("Streamlit UI")
+st.caption("시나리오를 Shot 단위로 분해하고, AI로 영화 스토리보드를 생성하세요.")
 
 
 
