710f24cd712bf83621111b788354275c34ef787b
HyeBin-Hub <gpqls00415@gmail.com>
2025-12-12 20:23:06 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,24 @@
-ㅇㅇ
+import times
+import streamlit 
+
+streamlit.set_page_config(
+  page_title = "Storyborad Generator",
+  layout = "wide"
+)
+
+######################
+# Header 설정 
+######################
+streamlit.title("Storyboard Generator")
+streamlit.caption("Streamlit UI")
+
+
+
+
+
+
+
+
+
+
+
