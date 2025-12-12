1518c5b3e846960f820f9a4fc62a4c36042fa775
HyeBin-Hub <gpqls00415@gmail.com>
2025-12-12 20:52:41 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,10 +19,12 @@ st.caption("시나리오를 Shot 단위로 분해하고, AI로 영화 스토리�
 col_left, col_right = st.columns([1, 2])
 
 with col_left:
-  with st.expander("Advanced Settings"):
-      st.header("Shot Settings")
+  with st.expander("Image Size Setting"):
+      # st.header("Shot Settings")
       width = st.selectbox("Width", [256, 512, 768, 1024], index = 1)
       height = st.selectbox("Height", [256, 512, 768, 1024], index = 1)
+    
+  with st.expander("Seed Setting"):
       steps = st.slider("Steps", 10, 40, 20)
       cfg = st.slider("CFG", 1.0, 15.0, 8.0, step=0.5)
     
@@ -32,6 +34,7 @@ with col_left:
       
 
 with col_right:
+
     st.header("Result")
     st.image("https://serverless-api-storage.runcomfy.net/deployment_requests/7f2ebf4d-ed40-4842-aba3-c91038b9cd36/output/ComfyUI_1765533995_00001_.png")
 
